ca2d0e911961350f03a1bf2db30ce676b6fee341
MuhammadHaikalFadlurrohman <haikalfadlurrohman@gmail.com>
2025-09-18 09:26:33 +0700

    Baru

---
+++ coba.js
@@ -0,0 +1,7 @@
+function cetakNama (nama) {
+    return `Halo, nama saya ${nama}`;
+}
+const PI = 3.14;
+
+Module.exports.cetakNama = cetakNama ;
+Module.exports.PI = PI;
---
+++ latihan1.js
@@ -0,0 +1,8 @@
+const cetakNama = require('./coba.js');
+console.log(cetakNama('Aaf'));
+
+const fs = require('fs');
+const coba = require('./coba.js');
+const moment = require('moment');
+
+console.log(coba.cetakNama('Aaf'), coba.PI);
